f31bad27e1ca6970bc8caf20c63b0bdc2749dbdb
Daniel <luffydafloffi@gmail.com>
2025-08-28 10:56:55 +0300

    Add Vue.js userscript app to the page

---
+++ lol.js
@@ -0,0 +1,44 @@
+(function () {
+    console.log("Running inside page context, Vue =", window.Vue);
+
+    const mountEl = document.createElement("div");
+    mountEl.id = "vue-userscript-app";
+    document.body.appendChild(mountEl);
+ 
+    const style = document.createElement("style");
+    style.textContent = `
+      #vue-userscript-app {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        background: white;
+        border: 1px solid #ccc;
+        padding: 10px;
+        z-index: 999999;
+      }
+   `;
+    document.head.appendChild(style);
+
+    const { createApp } = window.Vue;
+    const app = createApp({
+        setup() {
+            import { ref } from 'vue'
+
+            const token = 'blablah';
+            const ids = [62008, 61765, 61765];
+        },
+        template: `
+            <div style="display: flex; flex-direction: column; gap: 0.25rem;">
+                <a
+                  v-for="id of ids"
+                  :href="\`https://info.braude.ac.il/yedion/fireflyweb.aspx?PRGNAME=LOOK_FOR_NOSE&ARGUMENTS=-N206538449,'-N${window.someToken},-N${id}\`"
+                  :key="id"
+                >
+                    Course {{id}}
+                </a>
+            </div>
+        `,
+    });
+
+    app.mount("#vue-userscript-app");
+})();
